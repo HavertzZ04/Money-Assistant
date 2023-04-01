4713eaeccba5e55f4ea40f68ac8fed39f17323c5
HavertzZ04 <havertzz04@gmail.com>
2023-04-01 11:12:11 -0500

    feat: :sparkles: New fuctions
    I added more functions in order to add more rows on the tables automatelly and even calculate the %  of expenses of each row on the "expesesTables"

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let income = 0; //Money gotten
 let expenses = 0; //Money lost
 let percentageMain = 0; 
 
-
 let send = document.querySelector("#send");
 let select = document.querySelector("#selectValue");
 
@@ -26,13 +25,16 @@ send.addEventListener('click', function() {
         let tableIncome = document.querySelector("#tableIncome tbody");
         let description = document.querySelector("#description").value;
 
+        //Create the new elements when people add expenses
         let row = document.createElement("tr");
         let cell1 = document.createElement("td");
         let cell2 = document.createElement("td");
 
+        //Add values to the new elements
         cell1.textContent = description;
         cell2.textContent = `$ ${price}`;
 
+        //Add the new elements to the table
         row.appendChild(cell1);
         row.appendChild(cell2);
         tableIncome.appendChild(row);
@@ -49,29 +51,61 @@ send.addEventListener('click', function() {
         let tableExpenses = document.querySelector("#tableExpenses tbody");
         let description = document.querySelector("#description").value;
 
+        //Create the new elements when people add expenses
         let row = document.createElement("tr");
         let cell1 = document.createElement("td");
         let cell2 = document.createElement("td");
         let cell3 = document.createElement("td");
+        let deleteBtn = document.createElement("button");
 
+        //Add values to the new elements
         cell1.textContent = description;
         cell2.textContent = `$ ${price}`;
-        //cell3.textContent = (price * 100) / expenses
+        cell3.textContent = `${(price * 100) / expenses}%`;
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("delete-btn")
 
+        //Add the new elements to the table
         row.appendChild(cell1);
         row.appendChild(cell2);
-        //row.appendChild(cell3);
+        row.appendChild(cell3);
+        row.appendChild(deleteBtn);
         tableExpenses.appendChild(row);
+
+        //This function calculate the % for each row about the variable "expenses" and create a button to eliminate rows if the user wanna do it
+        function updatePercentage() {
+
+            //In this code we are calculating the % for each row
+            let rows = document.querySelectorAll("#tableExpenses tbody tr");
+            rows.forEach((row) => {
+              let price = parseFloat(row.querySelector("td:nth-child(2)").textContent.slice(2));
+              let percentage = (price * 100) / expenses;
+              let cell3 = row.querySelector("td:nth-child(3)");
+          
+              cell3.textContent = `${percentage.toFixed(1)}%`;
+            });
+
+            //In this code we are creating the button to eliminate rows
+            let tableExpenses = document.querySelector("#tableExpenses tbody");
+            tableExpenses.addEventListener("click", function(e) {
+                if (e.target.classList.contains("delete-btn")) {
+                    e.target.closest("tr").remove();
+                }
+            }); 
+        }
+
+        updatePercentage();
     }
 
-    
-
+    //Here we are sending the updated variables values to the DOM
     document.querySelector("#fullAmount").innerHTML = amount;
     document.querySelector("#income").innerHTML = `$${income}`;
     document.querySelector("#expenses").innerHTML = `$${expenses}`;
-    document.querySelector("#percentageMain").innerHTML = `%${percentage.toFixed(1)}` ;
+    document.querySelector("#percentageMain").innerHTML = `%${percentage.toFixed(1)}`;
 
 });
+
+
 
 
 
